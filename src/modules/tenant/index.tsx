96533d35a48c46ee060554f46da79bc06eb64592
richardpromise <richardpromise013@gmail.com>
2024-09-27 12:47:00 +0100

    update

--- src/modules/tenant/index.tsx
+++ src/modules/tenant/index.tsx
@@ -8,7 +8,41 @@ const Tenants = () => {
     setheader("Tenants");
   }, []);
 
-  return <div>tenants</div>;
+  return (
+    <div>
+      <table className="border w-[80%] mx-auto ">
+        <thead className=" bg-red-500 border">
+          <tr className=" ">
+            <th className="border text-left">Tenant ID</th>
+            <th>Tenant Name</th>
+            <th>Tenant Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-gray-300 border">
+            <td>1</td>
+            <td>Tenant 1</td>
+            <td>123 Main St</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Tenant 2</td>
+            <td>456 Elm St</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Tenant 2</td>
+            <td>456 Elm St</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Tenant 2</td>
+            <td>456 Elm St</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Tenants;
